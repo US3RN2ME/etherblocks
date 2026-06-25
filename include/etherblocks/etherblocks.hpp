d5850dfd4de083cad493510e9ce17b6ea4e4de85
username <username@email.com>
2026-06-25 16:39:03 +0200

    Add CMake installation rules, build options, and configuration files for modular packaging and distribution

--- include/etherblocks/etherblocks.hpp
+++ include/etherblocks/etherblocks.hpp
@@ -2,4 +2,25 @@
 #ifndef ETHERBLOCKS_HPP
 #define ETHERBLOCKS_HPP
 
+#include <etherblocks/engine/Camera.hpp>
+#include <etherblocks/engine/WorldChunks.hpp>
+#include <etherblocks/engine/WorldRendering.hpp>
+#include <etherblocks/engine/graphics/Buffer.hpp>
+#include <etherblocks/engine/graphics/Framebuffer.hpp>
+#include <etherblocks/engine/graphics/Material.hpp>
+#include <etherblocks/engine/graphics/Mesh.hpp>
+#include <etherblocks/engine/graphics/Renderer.hpp>
+#include <etherblocks/engine/graphics/Shader.hpp>
+#include <etherblocks/engine/graphics/Texture.hpp>
+#include <etherblocks/engine/graphics/VertexArray.hpp>
+#include <etherblocks/engine/ui/UiLayer.hpp>
+#include <etherblocks/game/Player.hpp>
+#include <etherblocks/game/Raycast.hpp>
+#include <etherblocks/game/World.hpp>
+#include <etherblocks/system/FrameClock.hpp>
+#include <etherblocks/system/Input.hpp>
+#include <etherblocks/system/Logger.hpp>
+#include <etherblocks/system/Utils.hpp>
+#include <etherblocks/system/Window.hpp>
+
 #endif // ETHERBLOCKS_HPP
